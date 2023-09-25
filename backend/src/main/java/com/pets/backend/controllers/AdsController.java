efaae66d907a6cf3dd8a0afdca66fa0ef1c43f32
AADS\scepan.jankovic <scepan.jankovic@material-exchange.com>
2023-09-25 09:23:16 +0200

    Small fix

--- backend/src/main/java/com/pets/backend/controllers/AdsController.java
+++ backend/src/main/java/com/pets/backend/controllers/AdsController.java
@@ -11,7 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "http://localhost:4200/")
+@CrossOrigin(origins = "http://localhost:4444/")
 @RestController
 @RequestMapping("/api")
 public class AdsController {
--- backend/src/main/java/com/pets/backend/controllers/FilesController.java
+++ backend/src/main/java/com/pets/backend/controllers/FilesController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "http://localhost:4200/")
+@CrossOrigin(origins = "http://localhost:4444/")
 @RestController
 @RequestMapping("/api")
 public class FilesController {
